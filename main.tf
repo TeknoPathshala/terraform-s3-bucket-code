06a0cec8fd83a641242bedf7e1665d1c2c66d289
TeknoPathshala <95009959+TeknoPathshala@users.noreply.github.com>
2021-12-24 11:54:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 #1 -this will create a S3 bucket in AWS
 resource "aws_s3_bucket" "terraform_state_s3" {
   #make sure you give unique bucket name
-  bucket = "terraform-coachdevops-state" 
+  bucket = "terraform-teknobucket-state" 
   force_destroy = true
 # Enable versioning to see full revision history of our state files
   versioning {
