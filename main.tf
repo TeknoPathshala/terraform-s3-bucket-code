fb62ae2e80e8885a04d8383833f8b7f54baf2735
admin <admin@tekno.com>
2021-12-24 01:09:02 -0500

    modify code

--- main.tf
+++ main.tf
@@ -42,23 +42,8 @@ terraform {
     key            = "dc/s3/terraform.tfstate"
     region         = "us-east-2"
     #Replace this with your DynamoDB table name!
-    dynamodb_table = "tf-up-and-run-locks"
+    dynamodb_table = "terraform-up-and-run-locks"
     encrypt        = true
     }
 }
 
-/*
-
-terraform {
-  backend "s3" {
-    #Replace this with your bucket name!
-    bucket         = "terraform-coachdevops-state"
-    key            = "dc/s3/terraform.tfstate"
-    region         = "us-east-2"
-    #Replace this with your DynamoDB table name!
-    dynamodb_table = "tf-up-and-run-locks"
-    encrypt        = true
-    }
-}
-
-*/
