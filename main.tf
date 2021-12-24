3b2e64b08b6a7b1f37edb3d72da6eb06833a04a3
TeknoPathshala <95009959+TeknoPathshala@users.noreply.github.com>
2021-12-24 11:50:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 #1 -this will create a S3 bucket in AWS
 resource "aws_s3_bucket" "terraform_state_s3" {
   #make sure you give unique bucket name
-  bucket = "terraform-newbucket-state" 
+  bucket = "terraform-coachdevops-state" 
   force_destroy = true
 # Enable versioning to see full revision history of our state files
   versioning {
@@ -27,25 +27,12 @@ server_side_encryption_configuration {
 # 2 - this Creates Dynamo Table
 resource "aws_dynamodb_table" "terraform_locks" {
 # Give unique name for dynamo table name
-  name         = "terraform-up-and-run-locks"
+  name         = "tf-up-and-run-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
         attribute {
          name = "LockID"
          type = "S"
       }
-}
-
-#Step 3 - Creates S3 backend
-terraform {
-  backend "s3" {
-    #Replace this with your bucket name!
-    bucket         = "terraform-newbucket-state"
-    key            = "dc/s3/terraform.tfstate"
-    region         = "us-east-2"
-    #Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-run-locks"
-    encrypt        = true
-    }
 }
 
