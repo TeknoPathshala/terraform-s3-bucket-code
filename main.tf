740360dd6628cd3ae27ea64323bcee89699d4dd7
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-07-19 19:52:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,25 +14,4 @@ resource "aws_s3_bucket" "terraform_state_s3" {
          enabled = true
         }
 
-# Enable server-side encryption by default
-server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-# 2 - this Creates Dynamo Table
-resource "aws_dynamodb_table" "terraform_locks" {
-# Give unique name for dynamo table name
-  name         = "TerraFom1-up-and-run-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-        attribute {
-         name = "LockID"
-         type = "S"
-      }
-}
 
