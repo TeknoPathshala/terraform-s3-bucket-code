6b13b97857fa4b7335fac833990bcd3d7d1cd94a
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-12 18:06:54 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+
+region ="us-east-2"
+
+}
+
+resource "aws_s3_bucket" "examples" {
+  bucket = "my-exampe-bucket"
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.examples.id
+  acl    = "private"
+}
