2beb640d7b339ddeadbbb729a254e4ad47676945
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-16 14:56:20 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "SecurityGroupsByNYM"{
          from_port = 0
          to_port   = 65535
          protocol = "tcp"
-         cidr_block = ["0.0.0.0/0"]
+         cidr_blocks = ["0.0.0.0/0"]
      }
 
      tags = {
