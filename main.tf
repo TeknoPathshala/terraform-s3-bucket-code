7851be6fc0f3c2c43a3a590cc58673ec9f3e42dc
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-17 17:00:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-resource "aws_instance" "ec2_1" {
+resource "aws_instance" "web" {
   ami           = "ami-0cea098ed2ac54925"
   instance_type = "t2.micro"
-  subnet_id     = data.aws_subnet.selected.id
-  key_name = var.my_key
+
   tags = {
     Name = "HelloWorld"
   }
