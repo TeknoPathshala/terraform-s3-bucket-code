c83b82184463c0836baff50b2f6ec28999d2d209
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-19 12:54:18 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,13 +2,13 @@
 
 #region ="us-east-2"
 
-}
+#}
 
-resource "aws_s3_bucket" "examples" {
-  bucket = "my-exampel-bucket"
-}
+#resource "aws_s3_bucket" "examples" {
+ # bucket = "my-exampel-bucket"
+#}
 
-resource "aws_s3_bucket_acl" "example_bucket_acl" {
-  bucket = aws_s3_bucket.examples.id
-  acl    = "private"
-}
+#resource "aws_s3_bucket_acl" "example_bucket_acl" {
+ # bucket = aws_s3_bucket.examples.id
+  #acl    = "private"
+#}
