732b7c2344aec5093dedea290e91ebc2636ec5a8
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-19 13:03:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_route_table" "example" {
 resource "aws_route" "route" {
   route_table_id            = "aws_route_table.example.id"
   destination_cidr_block    = "0.0.0.0/0"
-gateway_id = aws_internet_gateway.igw.id
+  gateway_id                = aws_internet_gateway.igw.id
   depends_on                = [aws_route_table.rt]
 }
 
