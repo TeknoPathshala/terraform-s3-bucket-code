7b77316bf300c107a729cecc7603ea13988bcfe8
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-19 12:25:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,53 @@
    # Name = "HelloWorld"
   #}
 #}
+
+provider "aws" {
+  region = "us-west-2"
+
+}
+
+################vpc block###############
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+#############Internet gateway###############
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+#######Subnet###################
+resource "aws_subnet" "mysubnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Subnet"
+  }
+}
+
+############Route Table##############
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route = []
+  tags = {
+    Name = "example"
+  }
+}
+
+#######Route###############
+resource "aws_route" "route" {
+  route_table_id            = "aws_route_table.example.id"
+  destination_cidr_block    = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.igw.id
+  depends_on                = [aws_route_table.rt]
+}
+
